73b3cc2937ed13ecb8c107f731f6ae9bdfd8da3b
Gavin Engel <gavin@engel.com>
2015-09-16 22:52:07 -0400

    .

---
+++ README.md
@@ -0,0 +1,61 @@
+# halfpass.js
+
+[![npm version](https://badge.fury.io/js/halfpass.svg)](http://badge.fury.io/js/halfpass)
+
+A halfpass is half a password. It is a 4+ char string for appending to a memorized string.  Uses sha512 as hash algorithm.  The hash salt is your ssh private key *and* the current year.  Can also display a previous year's halfpass as well, in case you need to recover an old password.
+
+## What problem does `halfpass` solve?
+Using secure passwords for various websites can be tricky.  Instead of writing down passwords, which is insecure, you could instead commit to memory a single baseword and use this module to generate a yearly suffix for every website.
+
+* example: your password baseword is `foobarbaz`, and this year's gmail.com append is: `tU6+`.  Your gmail.com password is: `foobarbaztU6+`.  Next year, your password would use the same baseword, but have a different halfpass at the end.
+
+## Installing 
+The lazy approach to installing this requires sudo (root) access:
+```
+  sudo npm install -g halfpass
+```
+
+Of course, I do not recommend installing npm packages with sudo unless it's required.
+
+This is how I install CLI tools from npm (like halfpass) without sudo:
+
+1. perform these steps: [https://gist.github.com/gavinengel/1842179837823dc25730](https://gist.github.com/gavinengel/1842179837823dc25730)
+2. $ nodebin
+3. $ npm install --save halfpass
+
+## Usage
+```
+$ halfpass gmail.com
+copied 2015's halfpass for `gmail.com`: tU6+
+```
+
+for extra security, you can mix in extra base64 chars:
+```
+$ halfpass gmail.com -a=3
+copied 2015's halfpass for `gmail.com`: tU6+h*a
+```
+
+if you want a previous year's halfpass:
+```
+$ halfpass gmail.com -y=2014
+copied 2015's halfpass for `gmail.com`: H2s*
+```
+
+## TODO
+
+* allow string in place of private key?
+
+## Credits
+
+TODO
+
+## Alternatives
+These are alternative projects which can yield achieve a similar result:
+
+TODO
+
+## License
+
+([The MIT License](http://opensource.org/licenses/MIT))
+Copyright (c) 2015 Gavin Engel <<gavin@engel.com>>
+
---
+++ halfpass.js
@@ -0,0 +1,107 @@
+#!/usr/bin/env node
+
+// REQUIRES
+require('copy-paste').global()
+var _argv = require('minimist')(process.argv.slice(2));
+var tld = require('tldjs')
+var _fs = require('fs')
+var _createHash = require('sha.js')
+var bases = require('bases')
+
+
+// VARIABLES
+var halfpass = ''
+var out = console.log
+var _date = new Date()
+var year = _argv.y || _date.getFullYear()
+var addl = _argv.a || 0
+var domain = tld.getDomain(_argv._[0]) || _argv._[0];
+var saltPath = process.env['HOME'] + '/.ssh/id_rsa'
+var salt = getPrivateKey(_fs.readFileSync(saltPath,'utf8')) + year
+var maxes = [{"name":"lower", "max":26}, {"name":"upper", "max":26}, {"name":"digit", "max":10}] // we need to get for 'random' int, for lowercase, uppercase, digit, and special-char (shift+digit on std keyboard)  
+var rands = []
+var sha = _createHash('sha512')
+var specials = ' !"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~' // using the ordering of ascii table
+maxes.push({"name":"special", "max":specials.length})
+
+
+// FUNCTIONS
+function getPrivateKey(rawSalt) {
+  var key = ''
+  rawSalt.split(/\n/).forEach(function(each) {
+    if (each.indexOf('-----') === -1) {
+      key = key + each
+    }
+  }) 
+  return key
+}
+
+function complex2simple(complex) {
+  simple = []
+  complex.forEach(function(each) {
+    if (each.name == 'lower') {
+      simple.push(String.fromCharCode(97 + each.val))
+    }
+    else if (each.name == 'upper') {
+      simple.push(String.fromCharCode(65 + each.val))
+
+    }
+    else if (each.name == 'digit') {
+      simple.push(each.val)
+
+    }
+    else if (each.name == 'special') {
+      simple.push(specials.slice(each.val, each.val+1))
+
+    }
+    else if (each.name == 'base64') {
+      simple.push(each.base64)
+
+    }
+    else {
+      out('Error: unknown type: '+each.name)
+    }
+  })
+
+  return simple
+}
+
+function objSorter(a,b) {
+  return parseInt(a.dec, 10) - parseInt(b.dec, 10);
+}
+
+
+// FLOW
+// also, add additional base64 if requested
+for (var i = 0; i < addl; i++) {
+  newPair = {"name":"base64", "max":64}
+  maxes.push(newPair)
+}
+
+// fill rands
+maxes.forEach(function(pair) {
+  hex = sha.update(i+salt+domain, 'utf8').digest('hex').slice(0, 5)
+  dec = bases.fromBase16(hex)//parseInt(hex, 16)
+  ///starts.push(i * size)
+  max = pair.max
+  newPair = {"val":dec % max, "name":pair.name, "dec":dec, "base64": bases.toBase64(dec)}
+  rands.push(newPair)
+  i++
+})
+
+// sort by the 'dec' value
+rands.sort(objSorter)
+
+// parse into single array
+simple = complex2simple(rands)
+
+// concatenate to produce final halfpass
+simple.forEach(function(each) {
+  halfpass = halfpass + each
+})
+
+
+// DONE
+copy(halfpass)
+out('copied ' + year + '\'s halfpass for `' + domain + '`: ' + halfpass)
+process.exit()
